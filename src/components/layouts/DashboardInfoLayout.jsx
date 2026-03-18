62b43e447aadfc1e34f42fb0f8fa783169116631
o6ez9na <kikikippplo@gmail.com>
2026-03-18 11:48:32 +0300

    fix adaptive styles

--- src/components/layouts/DashboardInfoLayout.jsx
+++ src/components/layouts/DashboardInfoLayout.jsx
@@ -26,9 +26,10 @@ export default function DashboardInfoLayout({
       <Grid
         templateColumns={{
           base: "1fr",
-          sm: "repeat(2, 1fr)",
-          md: "repeat(3, 1fr)",
-          lg: "repeat(4, 1fr)",
+          sm: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+          "2xl": "repeat(4, 1fr)",
         }}
         gap="1.2rem"
         w="100%"
